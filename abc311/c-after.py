ca463db8e019b2fe03058cb1f076fffb8deebb74
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-07-25 20:14:40 +0900

    ABC211 C途中経過

--- abc311/c-after.py
+++ abc311/c-after.py
@@ -3,15 +3,17 @@ import sys
 N = int(input())
 B = [0] + list(map(int, input().split()))
 
-for i, b in enumerate(B):
-    if i > 0:
-        root = [i]
-        for j in range(N):
-            check_index = root[-1]
-            if B[check_index] in root:
-                start = root.index(B[check_index])
-                print(len(root[start:]))
-                print(" ".join([str(i) for i in root[start:]]))
-                sys.exit(0)
-            else:
-                root.append(B[check_index])
+start_points = B.copy()
+while len(start_points) > 1:
+    root = [start_points[-1]]
+    start_points.pop(-1)
+    for j in range(N):
+        check_index = root[-1]
+        if B[check_index] in root:
+            start = root.index(B[check_index])
+            print(len(root[start:]))
+            print(" ".join([str(i) for i in root[start:]]))
+            sys.exit(0)
+        else:
+            root.append(B[check_index])
+            start_points.remove(B[check_index])
