3b715521157ee98af8ca57bcb4996383255983f2
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-07-26 11:08:50 +0900

    鉄則A02 復習AC

--- tessoku-book/chapter01/a02.py
+++ tessoku-book/chapter01/a02.py
@@ -1,12 +1,3 @@
-N, X = map(int, input().split())
-A = list(map(int, input().split()))
-
-flag = False
-for a in A:
-    if a == X:
-        flag = True
-
-if flag:
-    print("Yes")
-else:
-    print("No")
+_, x = map(int, input().split())
+isXs = [a == x for a in map(int, input().split())]
+print("Yes") if max(isXs) else print("No")
