dc9d850bb4d41da6afd4abb6c152aa860b2f7524
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-02 17:25:27 +0900

    鉄則A21 AC

--- tessoku-book/chapter04/a21.py
+++ tessoku-book/chapter04/a21.py
@@ -1,34 +1,37 @@
 N = int(input())
 blocks = [tuple(map(int, input().split())) for i in range(N)]
 
-indexs = [i+1 for i in range(N)]
+
+# 操作したときに得られる点数
 def get_point(l_or_r, l, r, bs):
-    print(l, r, indexs[l:N-r])
     if l_or_r == 'r':
-        p, a = bs[N-r]
+        p, a = bs[N - r]
     else:
-        p, a = bs[l-1]
-    if l+1 <= p < N -r + 1: #  in indexs[l:N-r]:
+        p, a = bs[l - 1]
+    if l + 1 <= p < N - r + 1:
         return a
     return 0
 
+
 max_point = 0
-dp = [[0 for _ in range(N+1)] for _ in range(N+1)]
-for l in range(N+1):
-    for r in range(N+1):
+dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+# 全部左から取った場合のdp
+for l in range(1, N + 1):
+    dp[l][0] = dp[l - 1][0] + get_point("l", l, 0, blocks)
+
+# 全部右から取った場合のdp
+for r in range(1, N + 1):
+    dp[0][r] = dp[0][r - 1] + get_point("r", 0, r, blocks)
+
+for l in range(1, N + 1):
+    for r in range(1, N + 1):
         if l + r <= N:
-            if r == 0 and l == 0:
-                dp[l][r] = 0
-            elif r == 0:
-                dp[l][r] = dp[l-1][r] + get_point("l", l, r, blocks)
-            elif l == 0:
-                dp[l][r] = dp[l][r-1] + get_point("r", l, r, blocks)
-            else:
-                dp[l][r] = max(dp[l-1][r] + get_point("l", l, r, blocks), dp[l][r-1] + get_point("r", l, r, blocks))
+            dp[l][r] = max(dp[l - 1][r] + get_point("l", l, r, blocks), dp[l][r - 1] + get_point("r", l, r, blocks))
         if l + r == N:
             if dp[l][r] > max_point:
                 max_point = dp[l][r]
 
-for row in dp:
-    print(*row)
-print(max_point)
+# 操作が終わったとき(操作回数の合計がN)の得点を抽出
+end_operations = [dp[i][N - i] for i in range(N + 1)]
+print(max(end_operations))
