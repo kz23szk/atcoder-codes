1a457abaf717e98db706f95f15cd9342e4859552
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-10 14:59:41 +0900

    鉄則B11,12 復習AC

--- tessoku-book/chapter03/b11.py
+++ tessoku-book/chapter03/b11.py
@@ -4,28 +4,20 @@ A.sort()
 Q = int(input())
 
 
-def count_lower_elem(l, x):
-    lower_index = 0
-    upper_index = len(l) - 1
+def search(n, l):
+    left, right = 0, len(l) - 1
 
-    if l[lower_index] > x:
-        return 0
-    if l[upper_index] < x:
-        return len(l)
-
-    while True:
-        check_index = (upper_index + lower_index) // 2
-        if l[check_index] == x:
-            return check_index
-        elif l[check_index] < x:
-            if len(l) - 1 >= check_index + 1 and x < l[check_index + 1]:
-                return check_index + 1
-            lower_index = check_index
+    while right != left:
+        mid = (left + right) // 2
+        if l[mid] >= n:
+            right = mid
         else:
-            if 0 <= check_index - 1 and l[check_index - 1] < x:
-                return check_index
-            upper_index = check_index
+            left = mid + 1
+    if l[left] < n:
+        return left + 1
+    return left
 
 
-for i in range(Q):
-    print(count_lower_elem(A, int(input())))
+for _ in range(Q):
+    x = int(input())
+    print(search(x, A))
--- tessoku-book/chapter03/b12.py
+++ tessoku-book/chapter03/b12.py
@@ -1,19 +1,14 @@
 N = int(input())
+left, right = 0, 100000
 
-Left = 0.0
-Right = N
+calc = lambda x: x ** 3 + x
 
+while right - left >= 0.001:
+    mid = (left + right) / 2
 
-def calc(x):
-    return x ** 3 + x
-
-
-while calc((Left + Right) / 2) < N - 0.001 or N + 0.001 < calc((Left + Right) / 2):
-    Mid = (Left + Right) / 2
-    ans = calc(Mid)
-    if ans < N:
-        Left = Mid
+    if calc(mid) >= N:
+        right = mid
     else:
-        Right = Mid
+        left = mid
 
-print((Left + Right) / 2)
+print(right)
