36c01f7f2c6476a9606d70a073418c14f02b3045
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-10 12:16:48 +0900

    鉄則A66 復習AC

--- tessoku-book/chapter09/a66.py
+++ tessoku-book/chapter09/a66.py
@@ -1,32 +1,30 @@
 N, Q = map(int, input().split())
-queries = [tuple(map(int, input().split())) for _ in range(Q)]
+queries = [list(map(int, input().split())) for _ in range(Q)]
 
-par = [-1 for _ in range(N + 1)]
-siz = [1 for _ in range(N + 1)]
+par, siz = [-1 for _ in range(N + 1)], [1 for _ in range(N + 1)]
 
 
-# 根を返す
+# xの根を返す
 def root(x):
     while par[x] != -1:
         x = par[x]
     return x
 
 
-def unite(u, v):
-    root_u, root_v = root(u), root(v)
-    # 同一グループのときは処理しない
-    if root_u == root_v:
-        return
-    if siz[root_u] < siz[root_v]:
-        par[root_u] = root_v
-        siz[root_v] += siz[root_u]
-    else:
-        par[root_v] = root_u
-        siz[root_u] += siz[root_v]
-
-
 def same(u, v):
     return root(u) == root(v)
+
+
+def unite(u, v):
+    root_u, root_v = root(u), root(v)
+    if root_u == root_v:
+        return
+    if siz[root_u] > siz[root_v]:
+        par[root_v] = root_u
+        siz[root_u] += siz[root_v]
+    else:
+        par[root_u] = root_v
+        siz[root_v] += siz[root_u]
 
 
 for q, u, v in queries:
