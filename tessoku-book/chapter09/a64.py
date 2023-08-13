f24e4e88c7dc5e58eb451871724286f3aeeee5fe
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 15:31:32 +0900

    鉄則A64 復習AC

--- tessoku-book/chapter09/a64.py
+++ tessoku-book/chapter09/a64.py
@@ -1,34 +1,31 @@
 import heapq
 
 N, M = map(int, input().split())
-ad_list = [set() for i in range(N + 1)]
-for i in range(M):
+ad_list = [set() for _ in range(N + 1)]
+for _ in range(M):
     a, b, c = map(int, input().split())
     ad_list[a].add((b, c))
     ad_list[b].add((a, c))
 
-max_dist = 10 ** 10
-cur = [max_dist for i in range(N + 1)]
+MAX_DIST = 10 ** 9 + 1
+fixed = [False for _ in range(N + 1)]
+cur = [MAX_DIST for _ in range(N + 1)]
+
 cur[1] = 0
-priority_queue = []
-heapq.heappush(priority_queue, (0, 1))
-kakutei = [ False for _ in range(N+1)]
-
-while len(priority_queue) > 0:
-    _, pos = heapq.heappop(priority_queue)
-
-    if kakutei[pos] == True:
+queue = []
+heapq.heappush(queue, (0, 1))
+while len(queue) > 0:
+    dist, pos = heapq.heappop(queue)
+    if fixed[pos]:
         continue
-
-    kakutei[pos] = True
+    fixed[pos] = True
     for nex, dist in ad_list[pos]:
-        prev_cur = cur[nex]
-        cur[nex] = min(cur[nex], cur[pos] + dist)
-        if cur[nex] < prev_cur:
-            heapq.heappush(priority_queue, (cur[nex], nex))
+        if cur[nex] > cur[pos] + dist:
+            cur[nex] = cur[pos] + dist
+            heapq.heappush(queue, (cur[nex], nex))
 
-for i in range(1, N + 1):
-    if cur[i] == max_dist:
+for dist in cur[1:]:
+    if dist == MAX_DIST:
         print(-1)
     else:
-        print(cur[i])
+        print(dist)
