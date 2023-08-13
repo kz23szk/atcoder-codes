a1219e0dfb3df22402d591eb67046d5bd85d3c93
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 15:15:26 +0900

    鉄則A63 復習AC

--- tessoku-book/chapter09/a63.py
+++ tessoku-book/chapter09/a63.py
@@ -1,25 +1,21 @@
 from collections import deque
 
 N, M = map(int, input().split())
-
-# 隣接リストを作る
 ad_list = [set() for _ in range(N + 1)]
 for _ in range(M):
-    A, B = map(int, input().split())
-    ad_list[A].add(B)
-    ad_list[B].add(A)
+    a, b = map(int, input().split())
+    ad_list[a].add(b)
+    ad_list[b].add(a)
 
-dist = [-1 for i in range(N + 1)]
 queue = deque()
-queue.append(1)
+dist = [-1 for i in range(N + 1)]
 dist[1] = 0
+queue.append(1)
 while len(queue) > 0:
     pos = queue.popleft()
     for nex in ad_list[pos]:
         if dist[nex] == -1:
-            queue.append(nex)
             dist[nex] = dist[pos] + 1
+            queue.append(nex)
 
-for d in dist[1:]:
-    print(d)
-
+print(*dist[1:], sep="\n")
