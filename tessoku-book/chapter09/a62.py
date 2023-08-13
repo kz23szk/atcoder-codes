7c02f199cdd5bdfa5314bbe08bfaaeabf5ab012e
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 14:58:53 +0900

    鉄則A62 復習AC

--- tessoku-book/chapter09/a62.py
+++ tessoku-book/chapter09/a62.py
@@ -1,26 +1,25 @@
 import sys
 
 sys.setrecursionlimit(10 ** 9)
-
 N, M = map(int, input().split())
-ad_list = [set() for _ in range(N)]
-visited = [False for i in range(N)]
-
-# 隣接リストを作る
-for _ in range(M):
+ad_list = [set() for _ in range(N + 1)]
+for i in range(M):
     a, b = map(int, input().split())
-    ad_list[a - 1].add(b - 1)
-    ad_list[b - 1].add(a - 1)
+    ad_list[a].add(b)
+    ad_list[b].add(a)
+
+visited = [False for _ in range(N + 1)]
 
 
 def dfs(pos):
     visited[pos] = True
-    # 隣接点をすべて探索しにいき、いったことがなければ探索する
     for nex in ad_list[pos]:
         if not visited[nex]:
             dfs(nex)
 
 
-dfs(0)
-answer = "The graph is connected." if min(visited) else "The graph is not connected."
-print(answer)
+dfs(1)
+if min(visited[1:]) == True:
+    print("The graph is connected.")
+else:
+    print("The graph is not connected.")
