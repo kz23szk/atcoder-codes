8f55110419ce082ddbb1d8f97a1b31668c995662
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 15:38:27 +0900

    鉄則A65 復習AC

--- tessoku-book/chapter09/a65.py
+++ tessoku-book/chapter09/a65.py
@@ -1,14 +1,7 @@
 N = int(input())
-A = list(map(int, input().split()))
-buka = [set() for i in range(N+1)]
-for i, a in enumerate(A):
-    buka[a].add(i+2)
-
-dp = [0 for _ in range(N+1)]
-for i in range(1, N+1)[::-1]:
-    for b in buka[i]:
-        # その部下の部下の数と部下自身を加算する
-        dp[i] += dp[b] + 1
+A = [0, 0] + list(map(int, input().split()))
+dp = [0 for _ in range(N + 1)]
+for i in range(2, N + 1)[::-1]:
+    dp[A[i]] += dp[i] + 1
 
 print(*dp[1:])
-
