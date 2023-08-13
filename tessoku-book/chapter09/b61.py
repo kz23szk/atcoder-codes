434471f36c62e715bcfe8b4243301bb9022ed4f0
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 19:25:28 +0900

    鉄則B61 AC

--- tessoku-book/chapter09/b61.py
+++ tessoku-book/chapter09/b61.py
@@ -1,16 +1,15 @@
 N, M = map(int, input().split())
-friendship = [set() for _ in range(N)]
+ad_list = [set() for _ in range(N + 1)]
 for i in range(M):
     a, b = map(int, input().split())
-    friendship[a - 1].add(b - 1)
-    friendship[b - 1].add(a - 1)
+    ad_list[a].add(b)
+    ad_list[b].add(a)
 
-max_index = 0
-max_cnt = 0
-for i, f in enumerate(friendship):
-    cnt = len(f)
-    if cnt > max_cnt:
-        max_cnt = cnt
+max_friends = 0
+max_index = -1
+for i in range(1, N + 1):
+    if max_friends < len(ad_list[i]):
+        max_friends = len(ad_list[i])
         max_index = i
 
-print(max_index + 1)
+print(max_index)
