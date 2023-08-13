7b28e88ff9ba876866930b858f7144e43691593c
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-13 19:38:22 +0900

    鉄則B62 復習AC

--- tessoku-book/chapter09/b62.py
+++ tessoku-book/chapter09/b62.py
@@ -3,29 +3,30 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 
 N, M = map(int, input().split())
-# 隣接リストを作る
 ad_list = [set() for _ in range(N + 1)]
-for i in range(M):
+for _ in range(M):
     a, b = map(int, input().split())
     ad_list[a].add(b)
     ad_list[b].add(a)
 
-visited = [False] * (N + 1)
+prev_pos = [-1 for i in range(N + 1)]
+visited = [False for _ in range(N + 1)]
+
+
+def dfs(pos):
+    visited[pos] = True
+    for nex in ad_list[pos]:
+        if not visited[nex]:
+            prev_pos[nex] = pos
+            dfs(nex)
+
+
+dfs(1)
+pos = N
 path = []
-
-
-def dfs(pos, v, ad_l):
+while pos != -1:
     path.append(pos)
-    # ゴールに着いたら答えを出力して終了
-    if pos == N:
-        print(*path)
-        exit()
-    v[pos] = True
+    pos = prev_pos[pos]
 
-    for nex in ad_l[pos]:
-        if not v[nex]:
-            dfs(nex, v, ad_l)
-    path.pop()
-
-
-dfs(1, visited, ad_list)
+path.reverse()
+print(*path)
