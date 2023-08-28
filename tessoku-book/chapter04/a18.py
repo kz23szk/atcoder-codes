2d8bf925a65214166a775533f7108b7658bc69b6
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-28 11:18:27 +0900

    鉄則A18 復習AC

--- tessoku-book/chapter04/a18.py
+++ tessoku-book/chapter04/a18.py
@@ -1,20 +1,13 @@
-import itertools
-
 N, S = map(int, input().split())
 A = list(map(int, input().split()))
 
-dp = [[False for _ in range(S + 1)] for _ in range(len(A) + 1)]
+# 1次元配列でもできる
+dp = [False for i in range(S+1)]
+dp[0] = True
 
-dp[0][0] = True
+for a in A:
+    for i in range(S+1)[::-1]:
+        if i - a >= 0:
+            dp[i] = dp[i] or dp[i-a]
 
-for i in range(1, len(A) + 1):
-    for j in range(S + 1):
-        if dp[i - 1][j]:
-            # 選ぶ
-            if j + A[i - 1] <= S:
-                dp[i][j + A[i - 1]] = True
-            # 選ばない
-            dp[i][j] = True
-
-last_list = [row[-1] for row in dp]
-print("Yes") if max(last_list) else print("No")
+print("Yes") if dp[-1] else print("No")
