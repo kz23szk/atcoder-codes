ef37369c0e3c4c5e8178a1cd21d4d14e2a818862
Kazufumi Suzuki <skazufumi.a02525@gmail.com>
2023-08-30 20:47:34 +0900

    鉄則B16,17 復習AC

--- tessoku-book/chapter04/b16.py
+++ tessoku-book/chapter04/b16.py
@@ -1,14 +1,12 @@
-import math
-
 N = int(input())
 h = list(map(int, input().split()))
-dp = [math.inf for _ in h]
+
+INF = 10 ** 10
+dp = [INF for i in range(N)]
 dp[0] = 0
 dp[1] = abs(h[1] - h[0])
 
-for i in range(2, len(h)):
-    root1 = abs(h[i] - h[i-1]) + dp[i-1]
-    root2 = abs(h[i] - h[i - 2]) + dp[i - 2]
-    dp[i] = min(root1, root2)
+for i in range(2, N):
+    dp[i] = min(dp[i-1] + abs(h[i] - h[i-1]), dp[i-2] + abs(h[i] - h[i-2]))
 
 print(dp[-1])
--- tessoku-book/chapter04/b17.py
+++ tessoku-book/chapter04/b17.py
@@ -1,20 +1,26 @@
 N = int(input())
-H = [0] + list(map(int, input().split()))
-dp = [0 for _ in range(N + 1)]
-dp[2] = abs(H[1] - H[2])
-for i in range(3, N + 1):
-    dp[i] = min(dp[i - 1] + abs(H[i] - H[i - 1]), dp[i - 2] + abs(H[i] - H[i - 2]))
+h = [0] + list(map(int, input().split()))
+
+INF = 10 ** 10
+dp = [INF for i in range(N+1)]
+dp[0] = 0
+dp[1] = 0
+dp[2] = abs(h[2] - h[1])
+
+for i in range(3, N):
+    step1 = abs(h[i] - h[i-1])
+    step2 = abs(h[i] - h[i - 2])
+    dp[i] = min(step1 + dp[i-1], step2 + dp[i-2])
 
 path = [N]
 i = N
-# 経路の復元
 while i > 1:
-    if dp[i] - dp[i - 2] == abs(H[i - 2] - H[i]):
-        i -= 2
-    else:
+    if dp[i] - dp[i-1] == abs(h[i] - h[i-1]):
         i -= 1
+    else:
+        i -= 2
     path.append(i)
-path.reverse()
 
+path.reverse()
 print(len(path))
 print(*path)
